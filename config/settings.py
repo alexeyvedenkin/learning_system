098e5e92839b461768be90af387cb8a91741a65c
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-07-03 18:28:11 +0500

    создана модель User

--- config/settings.py
+++ config/settings.py
@@ -98,8 +98,8 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# AUTH_USER_MODEL = "users.User"
-#
+AUTH_USER_MODEL = "users.User"
+
 # LOGIN_REDIRECT_URL = "/"
 # LOGOUT_REDIRECT_URL = "/"
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Email')
+    phone = models.CharField(max_length=15, verbose_name='Телефон', blank=True, null=True)
+    city = models.CharField(max_length=25, verbose_name='Город', blank=True, null=True)
+    avatar = models.ImageField(upload_to='users/avatars', verbose_name='Аватар', blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
