27b4a5546620096acb6de73fa10aaa5fa6ba5ae2
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-07-03 22:00:43 +0500

    созданы модели Course и Lesson

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название курса')
+    preview = models.ImageField(upload_to='courses/previews', blank=True, null=True)
+    description = models.TextField(verbose_name='Содержание курса', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название урока')
+    theme = models.ForeignKey(Course, on_delete=models.SET_NULL, verbose_name='Из курса', blank=True, null=True)
+    description = models.TextField(verbose_name='Содержание урока', blank=True, null=True)
+    preview = models.ImageField(upload_to='lessons/previews', blank=True, null=True)
+    video_file = models.FileField(upload_to='lessons/videos', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
