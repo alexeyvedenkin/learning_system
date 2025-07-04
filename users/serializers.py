873259bee3de3bbcb87cd0e4d9f18fd65c1d2cb9
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-07-04 17:32:28 +0500

    обработка isort

--- users/serializers.py
+++ users/serializers.py
@@ -2,6 +2,7 @@ from rest_framework.serializers import ModelSerializer
 
 from users.models import User
 
+
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,6 @@ from rest_framework.routers import SimpleRouter
 from users.apps import UsersConfig
 from users.views import UserViewSet
 
-
 app_name = UsersConfig.name
 
 router = SimpleRouter()
