b5d74183a37e313992b2372d98c9d263da98a510
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-07-04 15:46:16 +0500

    добавлены поля в модель users, выполнена миграция

--- users/models.py
+++ users/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='Email')
+    first_name = models.CharField(max_length=25, verbose_name='Город', blank=True, null=True)
+    last_name = models.CharField(max_length=25, verbose_name='Город', blank=True, null=True)
     phone = models.CharField(max_length=15, verbose_name='Телефон', blank=True, null=True)
     city = models.CharField(max_length=25, verbose_name='Город', blank=True, null=True)
     avatar = models.ImageField(upload_to='users/avatars', verbose_name='Аватар', blank=True, null=True)
