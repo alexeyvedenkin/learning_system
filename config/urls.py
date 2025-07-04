05346d9006463122b2cc3a63bfdf1dde56980cfc
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-07-04 15:20:48 +0500

    корректировка config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('materials', include('materials.urls', namespace='materials')),
+    path('materials/', include('materials.urls', namespace='materials')),
 ]
