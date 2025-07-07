750ff1afe46a0e59c4b8509c040e99e7f66b3a29
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-07-08 00:10:57 +0500

    настройка админ панели

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from materials.models import Course, Lesson
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "preview", "description")
+    list_filter = ("name",)
+    search_fields = ("name", "description")
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "theme", "description", "preview", "video_file")
+    search_fields = ("theme", "title")
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "first_name", "last_name", "phone", "city", "get_groups")
+    list_filter = ("last_name",)
+    search_fields = ("last_name",)
+
+    def get_groups(self, obj):
+        return ", ".join([group.name for group in obj.groups.all()])
+
+    get_groups.short_description = "Groups"
