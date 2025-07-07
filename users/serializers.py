604a56811bd2ce1d882d0edd9cb3390d656da883
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-07-08 01:34:49 +0500

    настройка создания пользователя через Postman

--- users/serializers.py
+++ users/serializers.py
@@ -1,15 +1,21 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from users.models import User
 
 
 class UserSerializer(ModelSerializer):
+    # Добавляем поле password, чтобы оно стало доступным при создании пользователя
+    password = serializers.CharField(write_only=True)  # Доступно только для записи
+
     class Meta:
         model = User
-        fields = ('email', 'first_name', 'last_name', 'phone', 'city', 'is_active')
+        fields = ('email', 'first_name', 'last_name', 'phone', 'city', 'is_active', 'password')
 
     def create(self, validated_data):
-        user = User(**validated_data)
-        user.set_password(validated_data['password'])
-        user.save()
+        # Удаляем пароль из данных для создания пользователя
+        password = validated_data.pop('password')  # Извлекаем пароль
+        user = User(**validated_data)  # Создаем объект пользователя без пароля
+        user.set_password(password)  # Устанавливаем пароль здесь
+        user.save()  # Сохраняем пользователя
         return user
--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ from users.views import UserViewSet
 app_name = UsersConfig.name
 
 router = SimpleRouter()
-router.register('', UserViewSet)
+router.register('user', UserViewSet)
 
 urlpatterns = []
 
