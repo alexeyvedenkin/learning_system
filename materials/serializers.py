6e3bed107b0e007d4927d5740dd0fd67981b7c4a
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-07-07 20:49:22 +0500

    настроен метод для подсчета количества уроков в курсе

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,12 +1,21 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from materials.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+
+    # Добавляем новое поле для количества уроков
+    lesson_count = SerializerMethodField()  # Создаем поле для вычисляемого значения
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_lesson_count(self, obj):
+        """ Метод для получения количества уроков """
+        return obj.lesson_set.count()  # Используем обратное отношение для доступа к урокам и считаем их
 
 
 class LessonSerializer(ModelSerializer):
