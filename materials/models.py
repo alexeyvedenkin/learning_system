e24cb1cdbb9efa071e20db7b83db8ba7ab17643f
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-07-08 13:36:05 +0500

    настроен вывод информации обо всех уроках курса, выполнена миграция

--- materials/models.py
+++ materials/models.py
@@ -13,7 +13,10 @@ class Course(models.Model):
 
 class Lesson(models.Model):
     title = models.CharField(max_length=100, verbose_name='Название урока')
-    theme = models.ForeignKey(Course, on_delete=models.SET_NULL, verbose_name='Из курса', blank=True, null=True)
+    theme = models.ForeignKey(Course, related_name='lessons',
+                              on_delete=models.SET_NULL,
+                              verbose_name='Из курса', blank=True, null=True
+                              )
     description = models.TextField(verbose_name='Содержание урока', blank=True, null=True)
     preview = models.ImageField(upload_to='lessons/previews', blank=True, null=True)
     video_file = models.FileField(upload_to='lessons/videos', blank=True, null=True)
--- materials/serializers.py
+++ materials/serializers.py
@@ -4,10 +4,18 @@ from rest_framework.serializers import ModelSerializer
 from materials.models import Course, Lesson
 
 
+class LessonSerializer(ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(ModelSerializer):
 
     # Добавляем новое поле для количества уроков
     lesson_count = SerializerMethodField()  # Создаем поле для вычисляемого значения
+    # Добавляем список уроков
+    lessons = LessonSerializer(many=True, read_only=True)
 
     class Meta:
         model = Course
@@ -15,10 +23,4 @@ class CourseSerializer(ModelSerializer):
 
     def get_lesson_count(self, obj):
         """ Метод для получения количества уроков """
-        return obj.lesson_set.count()  # Используем обратное отношение для доступа к урокам и считаем их
-
-
-class LessonSerializer(ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = '__all__'
+        return obj.lessons.count()  # Используем обратное отношение для доступа к урокам и считаем их
