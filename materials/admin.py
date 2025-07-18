613559f3a4b969b2def67d1eeade3220248fd1c2
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-07-18 16:49:03 +0500

    настройка админ панели

--- materials/admin.py
+++ materials/admin.py
@@ -5,12 +5,21 @@ from materials.models import Course, Lesson
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "preview", "description")
+    list_display = ("id", "name", "owner", "preview", "description")
     list_filter = ("name",)
     search_fields = ("name", "description")
 
 
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "theme", "description", "preview", "video_file")
+    list_display = ("id", "title", "theme", "owner", "description", "preview", "video_file")
     search_fields = ("theme", "title")
+
+    # def get_theme(self, obj):
+    #     # Lesson.objects.get(id=obj.id).theme.name
+    #     print(type(obj.theme[0].name))
+    #     return Course.objects.filter(lessons__id=obj.id).name
+
+    @admin.display(description='Курс', ordering='theme__name')
+    def get_theme(self, obj):
+        return obj.theme.name
