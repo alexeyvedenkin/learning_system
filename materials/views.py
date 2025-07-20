46288751511e1eecd94ba02b1488c9bddeeb27c8
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-07-20 17:29:08 +0500

    настройка прав доступа к модели materials

--- materials/views.py
+++ materials/views.py
@@ -3,16 +3,32 @@ from rest_framework.viewsets import ModelViewSet
 
 from materials.models import Course, Lesson
 from materials.serializers import CourseSerializer, LessonSerializer
+from users.permissions import IsModer
 
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+    def perform_create(self, serializer):
+        course =serializer.save(owner=self.request.user)
+        course.save()
+
+    def get_permissions(self):
+        if self.action in ["create", "destroy"]:
+            self.permission_classes = (~IsModer,)
+        else:
+            self.permission_classes = (IsModer,)
+        return super().get_permissions()
+
 
 class LessonCreateApiView(CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
+    def perform_create(self, serializer):
+        lesson =serializer.save(owner=self.request.user)
+        lesson.save()
 
 
 class LessonListApiView(ListAPIView):
--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -12,8 +13,8 @@ router = SimpleRouter()
 router.register('user', UserViewSet)
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
 
     path('payments/', PaymentListApiView.as_view(), name='payments_list'),
     path('payments/create/', PaymentCreateApiView.as_view(), name='payments_create'),
