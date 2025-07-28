813f33672bc4a3c9da9f7d6f039e84fc59f3d8c5
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-07-29 01:08:48 +0500

    настройка валидации для прикрепляемых ссылок в приложении materials

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,22 +1,40 @@
+import re
+
 from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from materials.models import Course, Lesson
-from .validators import validate_youtube_link  # Импортируем наш валидатор
+from .validators import validate_youtube_link  # Импортируем валидатор
+
+
+# Регулярное выражение для проверки ссылок на YouTube
+YOUTUBE_URL_PATTERN = r'https?://(www\.)?(youtube\.com|youtu\.be)/'
 
 
 class LessonSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=100)
     content = serializers.CharField()
-    video_url = serializers.URLField(validators=[validate_youtube_link])  # Используем функцию-валидатор
+    video_url = serializers.URLField(validators=[validate_youtube_link], required=False)  # Обозначаем, что это поле необязательное
 
     class Meta:
         fields = ['title', 'content', 'video_url']
 
+    def validate(self, attrs):
+        # Проверяем все ссылки в title и content
+        self.validate_youtube_links_in_text(attrs.get('title'))
+        self.validate_youtube_links_in_text(attrs.get('content'))
+        return attrs
+
+    def validate_youtube_links_in_text(self, text):
+        """ Проверка на наличие YouTube ссылок в тексте. """
+        if text:  # Проверяем, если текст присутствует
+            links = re.findall(YOUTUBE_URL_PATTERN, text)  # Находим все ссылки
+            for link in links:  # Проходим по каждой ссылке
+                validate_youtube_link(link[0])  # Вызов валидатора для каждой ссылки
+
 
 class CourseSerializer(ModelSerializer):
-
     # Добавляем новое поле для количества уроков
     lesson_count = SerializerMethodField()
     # Добавляем список уроков
@@ -31,16 +49,19 @@ class CourseSerializer(ModelSerializer):
         return obj.lessons.count()  # Используем обратное отношение для доступа к урокам и считаем их
 
 
-    class CourseSerializer(ModelSerializer):
-        # Добавляем новое поле для количества уроков
-        lesson_count = SerializerMethodField()
-        # Добавляем список уроков
-        lessons = LessonSerializer(many=True, read_only=True)
+    def validate(self, attrs):
+        for lesson in attrs.get('lessons', []):
+            video_url = lesson.get('video_url')
+            if video_url:  # Проверка для видео-ссылки
+                validate_youtube_link(video_url)
+        # Проверяем все ссылки в name и description курса
+        self.validate_youtube_links_in_text(attrs.get('name'))
+        self.validate_youtube_links_in_text(attrs.get('description'))
+        return attrs
 
-        class Meta:
-            model = Course
-            fields = '__all__'
-
-        def get_lesson_count(self, obj):
-            """ Метод для получения количества уроков """
-            return obj.lessons.count()  # Используем обратное отношение для доступа к урокам и считаем их
+    def validate_youtube_links_in_text(self, text):
+        """ Проверка на наличие YouTube ссылок в тексте. """
+        if text:
+            links = re.findall(YOUTUBE_URL_PATTERN, text)  # Найти все YouTube ссылки
+            for link in links:  # Проходим по каждой ссылке
+                validate_youtube_link(link[0])  # Вызов валидатора для каждой ссылки
