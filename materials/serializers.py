1be3ffc6b8725d2ca2bd0984fcda3885b1ceedee
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-07-28 23:54:16 +0500

    настройка валидации для прикрепляемых ссылок в приложении materials

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,13 +1,18 @@
+from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from materials.models import Course, Lesson
+from .validators import validate_youtube_link  # Импортируем наш валидатор
 
 
-class LessonSerializer(ModelSerializer):
+class LessonSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=100)
+    content = serializers.CharField()
+    video_url = serializers.URLField(validators=[validate_youtube_link])  # Используем функцию-валидатор
+
     class Meta:
-        model = Lesson
-        fields = '__all__'
+        fields = ['title', 'content', 'video_url']
 
 
 class CourseSerializer(ModelSerializer):
@@ -24,3 +29,17 @@ class CourseSerializer(ModelSerializer):
     def get_lesson_count(self, obj):
         """ Метод для получения количества уроков """
         return obj.lessons.count()  # Используем обратное отношение для доступа к урокам и считаем их
+
+    class CourseSerializer(ModelSerializer):
+        # Добавляем новое поле для количества уроков
+        lesson_count = SerializerMethodField()
+        # Добавляем список уроков
+        lessons = LessonSerializer(many=True, read_only=True)
+
+        class Meta:
+            model = Course
+            fields = '__all__'
+
+        def get_lesson_count(self, obj):
+            """ Метод для получения количества уроков """
+            return obj.lessons.count()  # Используем обратное отношение для доступа к урокам и считаем их
