72c360bc7307354699c7801f08591672e1a68033
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-08-06 23:24:42 +0500

    настройка url для использования celery

--- config/settings.py
+++ config/settings.py
@@ -149,7 +149,7 @@ CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
 
 # Часовой пояс для работы Celery
-CELERY_TIMEZONE = "Australia/Tasmania"
+CELERY_TIMEZONE = TIME_ZONE
 
 # Флаг отслеживания выполнения задач
 CELERY_TASK_TRACK_STARTED = True
