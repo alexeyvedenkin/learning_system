461597d690995628ca07e31f8c403beeb626a7c2
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-08-08 14:17:18 +0500

    Настроены отложенные задачи отправки подписчикам уведомлений об обновлении курса и урока

--- materials/tasks.py
+++ materials/tasks.py
@@ -1,6 +1,17 @@
 from celery import shared_task
+from django.core.mail import send_mail
+
+@shared_task
+def send_course_update_email(email_list, course_name):
+    """ Отправляет уведомление об обновлении курса """
+    subject = f"Обновление курса: {course_name}"
+    message = f"Курс '{course_name}' был обновлён. Проверьте его!"
+    send_mail(subject, message, from_email="from@example.com", recipient_list=email_list)
 
 
 @shared_task
-def add(x, y):
-    return x + y
+def send_lesson_update_email(email_list, lesson_name):
+    """ Отправляет уведомление об обновлении урока """
+    subject = f"Обновление урока: {lesson_name}"
+    message = f"Урок '{lesson_name}' был обновлён. Проверьте его!"
+    send_mail(subject, message, from_email="from@example.com", recipient_list=email_list)
